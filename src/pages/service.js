02068a1b94c2d3180017f7a08f5f67bc12a7dd21
xJREB <justus.bogner@gmail.com>
2024-09-17 10:36:10 +0200

    Updated service page

--- src/pages/service.js
+++ src/pages/service.js
@@ -17,6 +17,10 @@ const Service = () => {
                         <h3>Program Committee Activities</h3>
                         <ul>
                             <li>
+                                2025 3rd International Workshop on Responsible
+                                AI Engineering (RAIE)
+                            </li>
+                            <li>
                                 2025 22nd International Conference on Software
                                 Architecture (ICSA), Research track
                             </li>
